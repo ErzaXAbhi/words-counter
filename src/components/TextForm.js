0c3f4aee2f518492ef5b7188bbe2b6ae74b86a0f
Abhijit Parad <94881938+ErzaXAbhi@users.noreply.github.com>
2023-10-24 07:15:23 +0530

    up

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -20,6 +20,17 @@ export default function TextForm(props) {
     setText(e.target.value);
   };
 
+  const handleCopy = () => {
+    let text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+
   const [text, setText] = useState("");
   return (
     <>
@@ -42,6 +53,12 @@ export default function TextForm(props) {
         </button>
         <button className="btn btn-dark" onClick={handleClearClick}>
           Clear
+        </button>
+        <button className="btn btn-dark mx-2" onClick={handleCopy}>
+          Copy text
+        </button>
+        <button className="btn btn-dark mx-2" onClick={handleExtraSpaces}>
+          Remove extra spaces
         </button>
       </div>
       <div className="container my-2">
